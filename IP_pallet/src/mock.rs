e2fac8d597cc0b5a25604766653cf30ba91d3151
luke <daiagi@gmail.com>
2024-11-10 13:53:06 +0700

    add pallet balances

--- IP_pallet/src/mock.rs
+++ IP_pallet/src/mock.rs
@@ -6,21 +6,44 @@ use sp_runtime::{
     traits::{BlakeTwo256, IdentityLookup},
     BuildStorage,
 };
+use pallet_balances;
+use frame_support::traits::ConstU32;
 
 type Block = frame_system::mocking::MockBlock<Test>;
+pub type Balance = u128;
 
-// Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
     pub enum Test {
         System: frame_system,
+        Balances: pallet_balances,
         IPPallet: pallet,
     }
 );
+
 parameter_types! {
     pub const BlockHashCount: u64 = 250;
     pub const SS58Prefix: u8 = 42;
     pub const MaxNameLength: u32 = 50;
     pub const MaxDescriptionLength: u32 = 200;
+    pub const ExistentialDeposit: Balance = 1;
+    pub const MaxLocks: u32 = 50;
+    pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
+    type Balance = Balance;
+    type DustRemoval = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type ReserveIdentifier = [u8; 8];
+    type RuntimeHoldReason = ();
+    type FreezeIdentifier = ();
+    type MaxLocks = MaxLocks;
+    type MaxReserves = MaxReserves;
+    type MaxFreezes = ConstU32<0>;
+    type RuntimeFreezeReason = ();
 }
 
 impl system::Config for Test {
@@ -38,7 +61,7 @@ impl system::Config for Test {
     type BlockHashCount = BlockHashCount;
     type Version = ();
     type PalletInfo = PalletInfo;
-    type AccountData = ();
+    type AccountData = pallet_balances::AccountData<Balance>;
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
@@ -57,7 +80,7 @@ impl system::Config for Test {
 
 impl pallet::Config for Test {
     type RuntimeEvent = RuntimeEvent;
-    type Currency = ();
+    type Currency = Balances;
     type NFTId = u32;
     type OfferId = u32;
     type ContractId = u32;
@@ -67,10 +90,21 @@ impl pallet::Config for Test {
 }
 
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let mut ext: sp_io::TestExternalities = system::GenesisConfig::<Test>::default()
+    let mut t = system::GenesisConfig::<Test>::default()
         .build_storage()
-        .unwrap()
-        .into();
+        .unwrap();
+
+    pallet_balances::GenesisConfig::<Test> {
+        balances: vec![
+            (1, 10_000),
+            (2, 20_000),
+            (3, 30_000),
+        ],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    let mut ext = sp_io::TestExternalities::new(t);
     ext.execute_with(|| System::set_block_number(1));
     ext
 }
