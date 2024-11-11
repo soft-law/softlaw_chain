616fec51ba1dd4694c687f4345459f71d433b755
luke <daiagi@gmail.com>
2024-11-11 10:23:32 +0700

    mit tests

--- IP_pallet/src/tests.rs
+++ IP_pallet/src/tests.rs
@@ -1,10 +1,10 @@
 mod util;
 
-// mod mint_nft_tests;
-mod offer_tests;
 mod accept_license_tests;
 mod accept_purchase_tests;
-mod expire_license_tests;
 mod complete_purchase_tests;
-mod periodic_payment_tests;
+mod expire_license_tests;
+mod mint_nft_tests;
+mod offer_tests;
 mod on_initialize_tests;
+mod periodic_payment_tests;
