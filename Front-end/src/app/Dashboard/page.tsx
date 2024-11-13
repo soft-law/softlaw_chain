52ed02c6d179e90d58a7676bda36bc55be76bbab
Wario <69741055+wariomx@users.noreply.github.com>
2024-11-13 12:40:50 +0700

    pre fetch false links nav bar

--- Front-end/src/app/Dashboard/page.tsx
+++ Front-end/src/app/Dashboard/page.tsx
@@ -2,9 +2,7 @@
 import Footer from "@/components/Footer";
 import AccountsProvider from "@/context/account";
 import InnovationProvider from "@/context/innovation";
-// import NavBar from "@/components/NavBar";
 import dynamic from "next/dynamic";
-import Link from "next/link";
 import Dash from "../../components/Dashboard/Dash"
 
 const NavBar = dynamic(() => import("@/components/NavBar"), {
@@ -17,9 +15,6 @@ export default function Ipsearch() {
         <div className="scrollable">
           <NavBar />
           <Dash />
-          <Link href={"/search"}>Ip Search</Link>
-          <Link href={"/collection"}> Collection</Link>
-          <Link href={"/nft"}> Nft</Link>
           <Footer />
         </div>
       </AccountsProvider>
