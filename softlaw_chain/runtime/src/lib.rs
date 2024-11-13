5b611c5c369ece06c5460b44e6a8251c945a983d
Johan Alexis Duque Cadena <johan.duque@gmail.com>
2024-11-13 23:36:10 +0100

    It's needed to keep the original spec_name in order to upgrade, otherwise the upgrade process fails

--- softlaw_chain/runtime/src/lib.rs
+++ softlaw_chain/runtime/src/lib.rs
@@ -188,10 +188,10 @@ impl_opaque_keys! {
 
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("softlaw_runtime"),
-    impl_name: create_runtime_str!("softlaw_runtime"),
+    spec_name: create_runtime_str!("parachain-template-runtime"),
+    impl_name: create_runtime_str!("parachain-template-runtime"),
     authoring_version: 1,
-    spec_version: 1,
+    spec_version: 2,
     impl_version: 0,
     apis: apis::RUNTIME_API_VERSIONS,
     transaction_version: 1,
